4c7715f82b33dec23b77343bc50d82ff64fe91af
cheBen <benitoyannick7@gmail.com>
2020-02-08 13:30:56 +0100

    MAJ

---
+++ src/AppData.ts
@@ -0,0 +1,8 @@
+// AppData.ts
+
+export class AppData {
+    constructor(
+        // tslint:disable-next-line:ban-types
+        public name: String
+    ) {}
+  }
--- src/app/localShop/localShop.page.ts
+++ src/app/localShop/localShop.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-localSop',
@@ -6,7 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['localShop.page.scss']
 })
 export class LocalShopPage {
+  images = [
 
-  constructor() {}
+  ];
 
+
+  constructor(
+    public afDB: AngularFireDatabase,
+    public afSG: AngularFireStorage
+  ) {
+    this.getImagesDatabase();
+  }
+
+  getImagesDatabase() {
+    this.afDB.list('Images').snapshotChanges(['child_added']).subscribe(images => {
+      images.forEach(image => {
+        this.getImagesStorage(image);
+      });
+    });
+  }
+
+  getImagesStorage(image: any) {
+    const imgRef = image.payload.exportVal().ref;
+    this.afSG.ref(imgRef).getDownloadURL().subscribe(imgUrl => {
+      console.log(imgUrl);
+      this.images.push({
+        name: image.payload.exportVal().name,
+        url: imgUrl
+      });
+    });
+  }
 }
