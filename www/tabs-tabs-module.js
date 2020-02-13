7e30e1bbc4c312b41bd6c6f6a1cafdb6f7f1e297
cheBen <benitoyannick7@gmail.com>
2020-02-13 14:44:46 +0100

    Maj

---
+++ www/tabs-tabs-module.js
@@ -0,0 +1,214 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tabs-tabs-module"],{
+
+/***/ "./node_modules/raw-loader/dist/cjs.js!./src/app/tabs/tabs.page.html":
+/*!***************************************************************************!*\
+  !*** ./node_modules/raw-loader/dist/cjs.js!./src/app/tabs/tabs.page.html ***!
+  \***************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("<ion-header>\r\n\r\n</ion-header>\r\n\r\n\r\n<ion-tabs>\r\n\r\n  <ion-tab-bar slot=\"bottom\">\r\n    <ion-tab-button tab=\"localShop\">\r\n      <ion-icon name=\"home\" color=\"success\"></ion-icon>\r\n      <ion-label>Shop</ion-label>\r\n    </ion-tab-button>\r\n\r\n    <ion-tab-button tab=\"qrCode\">\r\n      <ion-icon name=\"qr-scanner\" color=\"primary\"></ion-icon>\r\n      <ion-label>QR Code</ion-label>\r\n    </ion-tab-button>\r\n\r\n    <ion-tab-button tab=\"geoCatching\">\r\n      <ion-icon name=\"walk\"color=\"tertiary-shade\"></ion-icon>\r\n      <ion-label>Géo Maps</ion-label>\r\n    </ion-tab-button>\r\n\r\n    <ion-tab-button tab=\"profile\">\r\n      <ion-icon name=\"contact\"></ion-icon>\r\n      <ion-label>Profile</ion-label>\r\n    </ion-tab-button>\r\n  </ion-tab-bar>\r\n\r\n</ion-tabs>\r\n");
+
+/***/ }),
+
+/***/ "./src/app/tabs/tabs-routing.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/tabs/tabs-routing.module.ts ***!
+  \*********************************************/
+/*! exports provided: TabsPageRoutingModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TabsPageRoutingModule", function() { return TabsPageRoutingModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _tabs_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./tabs.page */ "./src/app/tabs/tabs.page.ts");
+
+
+
+
+var routes = [
+    {
+        path: 'tabs',
+        component: _tabs_page__WEBPACK_IMPORTED_MODULE_3__["TabsPage"],
+        children: [
+            {
+                path: 'profile',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: function () {
+                            return __webpack_require__.e(/*! import() | profile-profile-module */ "profile-profile-module").then(__webpack_require__.bind(null, /*! ../profile/profile.module */ "./src/app/profile/profile.module.ts")).then(function (m) { return m.ProfilePageModule; });
+                        }
+                    }
+                ]
+            },
+            {
+                path: 'localShop',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: function () {
+                            return __webpack_require__.e(/*! import() | localShop-localShop-module */ "localShop-localShop-module").then(__webpack_require__.bind(null, /*! ../localShop/localShop.module */ "./src/app/localShop/localShop.module.ts")).then(function (m) { return m.LocalShopPageModule; });
+                        }
+                    }
+                ]
+            },
+            {
+                path: 'qrCode',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: function () {
+                            return __webpack_require__.e(/*! import() | qrCode-qrCode-module */ "qrCode-qrCode-module").then(__webpack_require__.bind(null, /*! ../qrCode/qrCode.module */ "./src/app/qrCode/qrCode.module.ts")).then(function (m) { return m.QrCodePageModule; });
+                        }
+                    }
+                ]
+            },
+            {
+                path: 'geoCatching',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: function () {
+                            return __webpack_require__.e(/*! import() | geoCatching-geoCatching-module */ "geoCatching-geoCatching-module").then(__webpack_require__.bind(null, /*! ../geoCatching/geoCatching.module */ "./src/app/geoCatching/geoCatching.module.ts")).then(function (m) { return m.GeoCatchingPageModule; });
+                        }
+                    }
+                ]
+            },
+            {
+                path: 'login',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: function () {
+                            return __webpack_require__.e(/*! import() | auth-login-login-module */ "auth-login-login-module").then(__webpack_require__.bind(null, /*! ../auth/login/login.module */ "./src/app/auth/login/login.module.ts")).then(function (m) { return m.LoginPageModule; });
+                        }
+                    }
+                ]
+            },
+            {
+                path: '',
+                redirectTo: '/tabs/localShop',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/tabs/localShop',
+        pathMatch: 'full'
+    }
+];
+var TabsPageRoutingModule = /** @class */ (function () {
+    function TabsPageRoutingModule() {
+    }
+    TabsPageRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+            exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+        })
+    ], TabsPageRoutingModule);
+    return TabsPageRoutingModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/tabs/tabs.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/tabs/tabs.module.ts ***!
+  \*************************************/
+/*! exports provided: TabsPageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TabsPageModule", function() { return TabsPageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _tabs_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./tabs-routing.module */ "./src/app/tabs/tabs-routing.module.ts");
+/* harmony import */ var _tabs_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./tabs.page */ "./src/app/tabs/tabs.page.ts");
+
+
+
+
+
+
+
+var TabsPageModule = /** @class */ (function () {
+    function TabsPageModule() {
+    }
+    TabsPageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_2__["NgModule"])({
+            imports: [
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_1__["IonicModule"],
+                _angular_common__WEBPACK_IMPORTED_MODULE_3__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_4__["FormsModule"],
+                _tabs_routing_module__WEBPACK_IMPORTED_MODULE_5__["TabsPageRoutingModule"]
+            ],
+            declarations: [_tabs_page__WEBPACK_IMPORTED_MODULE_6__["TabsPage"]]
+        })
+    ], TabsPageModule);
+    return TabsPageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/tabs/tabs.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/tabs/tabs.page.scss ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3RhYnMvdGFicy5wYWdlLnNjc3MifQ== */");
+
+/***/ }),
+
+/***/ "./src/app/tabs/tabs.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/tabs/tabs.page.ts ***!
+  \***********************************/
+/*! exports provided: TabsPage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TabsPage", function() { return TabsPage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var TabsPage = /** @class */ (function () {
+    function TabsPage() {
+    }
+    TabsPage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-tabs',
+            template: tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(/*! raw-loader!./tabs.page.html */ "./node_modules/raw-loader/dist/cjs.js!./src/app/tabs/tabs.page.html")).default,
+            styles: [tslib__WEBPACK_IMPORTED_MODULE_0__["__importDefault"](__webpack_require__(/*! ./tabs.page.scss */ "./src/app/tabs/tabs.page.scss")).default]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], TabsPage);
+    return TabsPage;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tabs-tabs-module.js.map
