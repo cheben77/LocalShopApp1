ae38cb2883e0d22915f77b202100ca2a725ec562
cheBen <benitoyannick7@gmail.com>
2020-02-14 14:37:42 +0100

    Mise en route des services "Badges // profile"

--- src/app/api/profile.service.ts
+++ src/app/api/profile.service.ts
@@ -1,20 +1,47 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Badge } from '@ionic-native/badge/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  userId: string;
+  mail: string;
+  method: string;
 
   constructor(
-    
-  ) {
-
-   }uploadImage(image: string, userId: string): any {
+    public loadingController: LoadingController,
+    public alertController: AlertController,
+    private badge: Badge,
+    public afDB: AngularFireDatabase,
+    public afAuth: AngularFireAuth,
+    public afSG: AngularFireStorage,
+    private camera: Camera
+  ) {{
+    this.afAuth.authState.subscribe(auth => {
+      if (!auth) {
+        console.log('non connecté');
+      } else {
+        console.log('connecté: ' + auth.uid);
+        this.userId = auth.uid;
+        this.mail = auth.email;
+        this.method = auth.providerData[0].providerId;
+       // this.badge.set();
+       // this.badge.increase();
+       // this.badge.clear();
+      }
+    }); }}
+     uploadImage(Image: string, userId: string): any {
       // Implementation
     }
-  
-    getImage(userId: string, imageId: string): any {
+
+    // tslint:disable-next-line: no-unused-expression
+     getImage(userId: string, Image: string): any {
       // Implementation
     }
 
@@ -27,6 +54,4 @@ export class ProfileService {
       return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
         s4() + '-' + s4() + s4() + s4();
     }
-
-
 }
