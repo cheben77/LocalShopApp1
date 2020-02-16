54d0b6cfdd7eab46f28c74a822b82d3ccfc55119
cheBen <benitoyannick7@gmail.com>
2020-02-16 01:02:42 +0100

    Revert "Revert "Revert "Merge remote-tracking branch 'refs/remotes/origin/geocatching'"""
    This reverts commit 75d237926a345c26ec3970c7322d94bcdab52f60.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,17 @@ import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 
 
-import { environment } from '../environments/environment';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCcyuFkXAqca0vcsLIQRG2UociZGr2oUrA',
+    authDomain: 'geoapp-a10da.firebaseapp.com',
+    databaseURL: 'https://geoapp-a10da.firebaseio.com',
+    projectId: 'geoapp-a10da',
+    storageBucket: 'geoapp-a10da.appspot.com',
+    messagingSenderId: '279119677501',
+    appId: '1:279119677501:web:91d699c056a5869f92c254',
+    measurementId: 'G-SRN9Q6HXGX'
+};
 
 
 
@@ -34,7 +44,7 @@ import { environment } from '../environments/environment';
     IonicModule.forRoot(),
     QRCodeModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     AngularFireStorageModule
