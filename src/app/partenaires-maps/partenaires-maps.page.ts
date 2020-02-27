e1e5f88b48e5c78ac0110801ce87ea34cc3fe356
cheBen <benitoyannick7@gmail.com>
2020-02-27 15:52:16 +0100

    Mise en place de la page "Partenaire"

---
+++ src/app/partenaires-maps/partenaires-maps.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partenaires-maps',
+  templateUrl: './partenaires-maps.page.html',
+  styleUrls: ['./partenaires-maps.page.scss'],
+})
+export class PartenairesMapsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
