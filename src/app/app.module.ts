f112891b654d753399b84d73c9127e662e60c26d
cheBen <benitoyannick7@gmail.com>
2020-03-22 14:05:04 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,15 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { FirebaseService } from './services/firebase.service';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Platform } from '@ionic/angular';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
 
 import { QRCodeModule } from 'angularx-qrcode';
 import { QRScanner } from '@ionic-native/qr-scanner/ngx';
-import { environment } from 'src/environments/environment';import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
@@ -26,16 +31,16 @@ import { Observable } from 'rxjs';
 
 import { Facebook } from '@ionic-native/facebook/ngx';
 
-export const firebaseConfig = {
-    apiKey: 'AIzaSyCcyuFkXAqca0vcsLIQRG2UociZGr2oUrA',
-    authDomain: 'geoapp-a10da.firebaseapp.com',
-    databaseURL: 'https://geoapp-a10da.firebaseio.com',
-    projectId: 'geoapp-a10da',
-    storageBucket: 'geoapp-a10da.appspot.com',
-    messagingSenderId: '279119677501',
-    appId: '1:279119677501:web:91d699c056a5869f92c254',
-    measurementId: 'G-SRN9Q6HXGX'
-}
+// export const firebaseConfig = {
+    // apiKey: 'AIzaSyCcyuFkXAqca0vcsLIQRG2UociZGr2oUrA',
+    // authDomain: 'geoapp-a10da.firebaseapp.com',
+    // databaseURL: 'https://geoapp-a10da.firebaseio.com',
+    // projectId: 'geoapp-a10da',
+    // storageBucket: 'geoapp-a10da.appspot.com',
+    // messagingSenderId: '279119677501',
+    // appId: '1:279119677501:web:91d699c056a5869f92c254',
+    // measurementId: 'G-SRN9Q6HXGX'
+// }
 
 @NgModule({
   declarations: [AppComponent],
