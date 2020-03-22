a3ce3f530bf1f27bf360ea80381c2c22a60879c5
cheBen <benitoyannick7@gmail.com>
2020-03-22 11:01:21 +0100

    pacquage sup ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { QRCodeModule } from 'angularx-qrcode';
 import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+//import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { environment } from 'src/environments/environment';
@@ -30,6 +32,17 @@ import { Observable } from 'rxjs';
 
 import { Facebook } from '@ionic-native/facebook/ngx';
 
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCcyuFkXAqca0vcsLIQRG2UociZGr2oUrA',
+    authDomain: 'geoapp-a10da.firebaseapp.com',
+    databaseURL: 'https://geoapp-a10da.firebaseio.com',
+    projectId: 'geoapp-a10da',
+    storageBucket: 'geoapp-a10da.appspot.com',
+    messagingSenderId: '279119677501',
+    appId: '1:279119677501:web:91d699c056a5869f92c254',
+    measurementId: 'G-SRN9Q6HXGX'
+};
 
 @NgModule({
   declarations: [AppComponent],
